340dea5fb9b48be6a90b28246b10b4dec326cda4
mohamedhamedd <modeveloperr@gmail.com>
2021-09-18 04:37:28 +0200

    Initial commit

--- app/src/main/java/com/moapps/newsapp/breakingnews/utils/Constants.kt
+++ app/src/main/java/com/moapps/newsapp/breakingnews/utils/Constants.kt
@@ -2,7 +2,7 @@ package com.moapps.newsapp.breakingnews.utils
 
 object Constants {
 
-    const val BASE_URL = "https://newsapi-flask.herokuapp.com/"
+    const val BASE_URL = "BASE URL API HERE"
     const val ARGS_ARTICLE = "article"
     const val ARGS_SITE = "site"
     const val ARGS_BREAKING = "breaking"
